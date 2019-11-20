8d7d008078f4f2f2088a8e5b5e2bfc97bd933227
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-20 11:22:31 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,48 @@
-def hello_t
+  require 'pry'
 
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      #binding.pry
+      i = i + 1
+      #binding.pry
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
 
+
+
+
+
+# def hello_t
+#   arr = []
+#   counter == 1
+#   arr.cycle(1) do |name| name  += 1
+#   binding.pry
+#   puts "Hi, #{name}"
+#   counter += 1
+#   #binding.pry
+#   end
+# end
+
+
+
+
+# def hello_t
+  
+#     arr = []
+#     counter == 1
+  
+#     arr.cycle(1) do |name| name  += 1
+#       puts "Hi, #{name}"
+#     end
+#     counter += 1
+# end
